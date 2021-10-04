226b2a830f601d59e791a064332abf0f077f869e
Keval <kevalm221@gmail.com>
2021-10-04 23:09:28 +0530

    Add javascript for remove from cart

--- JS/script.js
+++ JS/script.js
@@ -11,8 +11,28 @@
    orderList = orderList.concat(str)+':' + price +'\n';
    console.log(orderList);
    console.log('total amount:'+total);
-   document.getElementById('amount').innerHTML = orderList + "\n" + "<b>Total Amount:</b>" + total;
-
-   
+   document.getElementById('amount').innerHTML = orderList + "\n" + "<b>Total Amount:</b>" + total; 
 }
 
+let removeCartItemButtons = document.getElementsByClassName('btn-danger')
+// console.log(removeCartItemButtons)
+for(var i = 0;i<removeCartItemButtons.length;i++){
+   var button = removeCartItemButtons[i];
+   button.addEventListener('click',function (event){
+     var buttonClicked =  event.target
+     buttonClicked.parentElement.parentElement.remove();
+     updateCartTotal()
+   })
+}
+
+function updateCartTotal(){
+   let cartItemContainer = document.getElementsByClassName('cart-item')[0]
+   let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+   for(var i = 0;i<cartRows.length;i++){
+      let cartRow = cartRows[i]
+      let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+      let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+      console.log(priceElement,quantityElement);
+   }
+
+}
