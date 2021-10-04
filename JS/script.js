63465719b4d5f525b2f39dae010945138b975184
Keval <kevalm221@gmail.com>
2021-10-04 14:31:19 +0530

    idk

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,20 @@
  var total = 0;
- var str = "";
-function clickHandler(elem){
+ let str;
+ function clickHandler(elem){
    // elem.preventDefault();
    // console.log(elem.parentNode.id);
    let parentId = elem.parentNode.id;
+   // console.log(parentId);
    // console.log($('parentId').children('.card-text.price').text());
 //   console.log($('#'+parentId).children('.price').text())
    let price =  parseInt($('#'+parentId).children('.price').text());
    total = total + price;
    console.log(total);
    document.getElementById('amount').innerHTML = total;
+   str = $('#'+parentId).children('.card-title').text();
+   // str=document.querySelector(".card-title").textContent;
+   console.log(str);
    // document.getElementById('modal-body').innerHTML = $('#card-title'),$('#price')
    
 }
+$('#pauhe').children('.card-title').text()
