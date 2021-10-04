0a377871e01214b01f51e05f67b81fcdc7ab1637
Keval <kevalm221@gmail.com>
2021-10-04 10:40:05 +0530

    idk

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,9 @@
 
 function clickHandler(elem){
   
-   console.log(elem.parentNode.id);
+   // console.log(elem.parentNode.id);
    let parentId = elem.parentNode.id;
    // console.log($('parentId').children('.card-text.price').text());
-  
-   console.log( parseInt($(parentId).children('.price').text()));
+//   console.log($('#'+parentId).children('.price').text())
+   console.log( parseInt($('#'+parentId).children('.price').text()));
 }
