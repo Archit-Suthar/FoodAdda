f5683ed89d190e10d14157d8fed86ca202140d2f
Archit Suthar <suthararchit8@gmail.com>
2021-10-04 11:08:09 +0530

    trial changes in order.html

--- JS/script.js
+++ JS/script.js
@@ -6,5 +6,9 @@ function clickHandler(elem){
    let parentId = elem.parentNode.id;
    // console.log($('parentId').children('.card-text.price').text());
 //   console.log($('#'+parentId).children('.price').text())
-   console.log( parseInt($('#'+parentId).children('.price').text()));
+   let price =  parseInt($('#'+parentId).children('.price').text());
+   total = total + price;
+   console.log(total);
+   document.getElementById('amount').innerHTML = total;
+  
 }
