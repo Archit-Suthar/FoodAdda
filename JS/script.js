6e61b860fe7faa1e5271998a40c45690e05c1f39
Keval <kevalm221@gmail.com>
2021-10-04 15:16:00 +0530

    add order List

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,18 @@
  var total = 0;
- let str;
+ let orderList = ""
  function clickHandler(elem){
-   // elem.preventDefault();
-   // console.log(elem.parentNode.id);
+    let str;
+  
    let parentId = elem.parentNode.id;
-   // console.log(parentId);
-   // console.log($('parentId').children('.card-text.price').text());
-//   console.log($('#'+parentId).children('.price').text())
+   
    let price =  parseInt($('#'+parentId).children('.price').text());
    total = total + price;
-   console.log(total);
-   document.getElementById('amount').innerHTML = total;
    str = $('#'+parentId).children('.card-title').text();
-   // str=document.querySelector(".card-title").textContent;
-   console.log(str);
-   // document.getElementById('modal-body').innerHTML = $('#card-title'),$('#price')
+   orderList = orderList.concat(str)+':' + price +'\n';
+   console.log(orderList);
+   console.log('total amount:'+total);
+   document.getElementById('amount').innerHTML = orderList + "\n" + "<b>Total Amount:</b>" + total;
+
    
 }
-$('#pauhe').children('.card-title').text()
+
