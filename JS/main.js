da6c3f30da0bf6a2387cdc37ffdcbd7975b50879
Keval <kevalm221@gmail.com>
2021-10-05 21:49:51 +0530

    Cart work testing

--- JS/main.js
+++ JS/main.js
@@ -62,11 +62,23 @@
 		};
 
 		function addToCart(event) {
+			var button = event.target
+			var shopItem = button.parentElement.parentElement
+			console.log(shopItem);
+			var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+			console.log(title)
+			var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+			console.log(price)
+			var ImageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+			console.log(ImageSrc)
 			event.preventDefault();
 			if(animatingQuantity) return;
 			var cartIsEmpty = Util.hasClass(cart[0], 'cd-cart--empty');
 			//update cart product list
-			addProduct(this);
+			productId = productId + 1;
+			var productAdded = `<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src=${ImageSrc} alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">${title}</a></h3><span class="cd-cart__price">${price}</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>`;
+			cartList.insertAdjacentHTML('beforeend', productAdded);
+			// addProduct(this);
 			//update number of items 
 			updateCartCount(cartIsEmpty);
 			//update total price
@@ -95,15 +107,7 @@
 			}
 		};
 
-		function addProduct(target) {
-			// this is just a product placeholder
-			// you should insert an item with the selected product info
-			// replace productId, productName, price and url with your real product info
-			// you should also check if the product was already in the cart -> if it is, just update the quantity
-			productId = productId + 1;
-			var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
-			cartList.insertAdjacentHTML('beforeend', productAdded);
-		};
+		
 
 		function removeProduct(product) {
 			if(cartTimeoutId) clearInterval(cartTimeoutId);
