277601b70d054a3e42055f62d3a2582ce1d2e588
Keval <kevalm221@gmail.com>
2021-10-05 10:41:55 +0530

    add customer review Carousel

--- JS/script.js
+++ JS/script.js
@@ -36,3 +36,15 @@ function updateCartTotal(){
    }
 
 }
+ 
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        autoPlay:true
+    });
+});
