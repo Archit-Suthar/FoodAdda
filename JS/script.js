9579e16721b0e7535bcb48d72503a3eded0fc655
Archit Suthar <suthararchit8@gmail.com>
2021-10-05 16:12:04 +0530

    updateTotal working

--- JS/script.js
+++ JS/script.js
@@ -1,50 +1,63 @@
- var total = 0;
- let orderList = ""
- function clickHandler(elem){
-    let str;
-  
-   let parentId = elem.parentNode.id;
-   
-   let price =  parseInt($('#'+parentId).children('.price').text());
-   total = total + price;
-   str = $('#'+parentId).children('.card-title').text();
-   orderList = orderList.concat(str)+':' + price +'\n';
-   console.log(orderList);
-   console.log('total amount:'+total);
-   document.getElementById('amount').innerHTML = orderList + "\n" + "<b>Total Amount:</b>" + total; 
+if (document.readyState == 'loading') {
+   document.addEventListener('DOMContentLoaded', ready)
+}
+else {
+   ready()
 }
 
-let removeCartItemButtons = document.getElementsByClassName('btn-danger')
-// console.log(removeCartItemButtons)
-for(var i = 0;i<removeCartItemButtons.length;i++){
-   var button = removeCartItemButtons[i];
-   button.addEventListener('click',function (event){
-     var buttonClicked =  event.target
-     buttonClicked.parentElement.parentElement.remove();
-     updateCartTotal()
-   })
-}
-
-function updateCartTotal(){
-   let cartItemContainer = document.getElementsByClassName('cart-item')[0]
-   let cartRows = cartItemContainer.getElementsByClassName('cart-row')
-   for(var i = 0;i<cartRows.length;i++){
-      let cartRow = cartRows[i]
-      let priceElement = cartRow.getElementsByClassName('cart-price')[0]
-      let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-      console.log(priceElement,quantityElement);
+function ready() {
+   var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+   for (var i = 0; i < removeCartItemButtons.length; i++) {
+      var button = removeCartItemButtons[i];
+      button.addEventListener('click', removeCartItem)
    }
 
+
+   var quantityInputs = document.getElementsByClassName('card-quantity-input')
+   for(var i=0; i < quantityInputs.length;i++){
+      var input = quantityInputs[i]
+      input.addEventListener('change',quantityChanged)
+   }
 }
- 
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:3,
-        itemsDesktop:[1000,3],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,2],
-        itemsMobile:[650,1],
-        pagination:true,
-        autoPlay:true
-    });
+
+function removeCartItem(event) {
+   var buttonClicked = event.target
+   buttonClicked.parentElement.parentElement.remove();
+   updateCartTotal()
+}
+
+function quantityChanged(event){
+   var input = event.target
+   if(isNaN(input.value) || input.value <= 0){
+      input.value = 1
+   }
+   updateCartTotal()
+}
+function updateCartTotal() {
+   var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+   var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+   var total = 0;
+   for (var i = 0; i < cartRows.length; i++) {
+      var cartRow = cartRows[i]
+      var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+      var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+      // console.log(priceElement,quantityElement);
+      var price = priceElement.innerText.replace('₹', '')
+      var quantity = quantityElement.value
+      total = total + price * quantity;
+   }
+   document.getElementsByClassName('cart-total-price')[0].innerText = total;
+
+}
+
+$(document).ready(function () {
+   $("#testimonial-slider").owlCarousel({
+      items: 3,
+      itemsDesktop: [1000, 3],
+      itemsDesktopSmall: [979, 2],
+      itemsTablet: [768, 2],
+      itemsMobile: [650, 1],
+      pagination: true,
+      autoPlay: true
+   });
 });
