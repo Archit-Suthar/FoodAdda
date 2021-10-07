5508c61e5d77d1d9180ce515c163e273d187e44b
Keval <kevalm221@gmail.com>
2021-10-07 13:02:01 +0530

    add green text

--- JS/main.js
+++ JS/main.js
@@ -237,7 +237,7 @@
 	 var orderModalBody = document.getElementById('orderModalBody');
 	// orderModalBody.innerHTML = orderConfirm.children;
 	
-	orderModalBody.innerHTML = orderConfirm + `<b style="float: right;">Total Amount: ${totalAmount}</b>`;
+	orderModalBody.innerHTML = orderConfirm + `<b style="float: right;">Total Amount: ₹${totalAmount}</b>`;
 	
   })
   var closeBtn = document.getElementById('actionEnable');
@@ -257,10 +257,15 @@
 	var orderConfirm = document.getElementsByClassName('cd-cart__body')[0].innerHTML;
 	 var code =  document.getElementById("promoCode").value
 	if(code == 'GET75OFF' && totalAmount>299){
-		orderModalBody.innerHTML = orderConfirm + `<b style="float: right;">Total Amount: ₹${totalAmount-75}</b>`;
+		orderModalBody.innerHTML = orderConfirm + `<b style="color:green;"> *Promocode Applied Successfully</b><b style="float: right;">Total Amount: ₹${totalAmount-75}</b>`;
 	}else if(code != 'GET75OFF' || totalAmount<299 ){
-		orderModalBody.innerHTML = orderConfirm + `<p style="float: right;"><b>Total Amount: ${totalAmount}</b></p><p style="color:red"><b> *Promocode not Applicable</b></p>`;
+		orderModalBody.innerHTML = orderConfirm + `<p style="float: right;"><b>Total Amount: ₹${totalAmount}</b></p><p style="color:red"><b> *Promocode not Applicable</b></p>`;
 	}
   } )
+
+//   var confirmed = document.getElementById('comfirmed');
+//   confirmed.addEventListener('click',function finalOrderPlaced(){
+// 	console.log("Ok!!")
+//   })
 })();
 
